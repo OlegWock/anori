306a0e254a40b1ea6ed10e97b549ffc9d115be3a
OlegWock <olegwock@gmail.com>
2023-02-18 18:27:52 +0100

    Remove prettier

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,7 +2,7 @@ module.exports = {
     root: true,
     parser: '@typescript-eslint/parser',
     plugins: ['@typescript-eslint'],
-    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
     rules: {
         'react/jsx-uses-react': 'off',
         'react/react-in-jsx-scope': 'off',
--- .prettierrc.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    trailingComma: 'es5',
-    printWidth: 120,
-    tabWidth: 4,
-    semi: true,
-    singleQuote: true,
-};
